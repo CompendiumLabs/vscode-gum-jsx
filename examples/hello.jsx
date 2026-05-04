45db617172c72e4e627a61b9e381a6a37ca1e8b4
Douglas Hanley <thesecretaryofwar@gmail.com>
2026-05-04 11:17:18 -0500

    fix example

--- examples/hello.jsx
+++ examples/hello.jsx
@@ -1,10 +1,10 @@
-<Frame padding={0.05}>
+<Box padding={0.05}>
     <VStack spacing={0.05}>
         <Text>Hello, gum.jsx!</Text>
         <HStack spacing={0.05}>
             <Circle fill={blue} />
             <Square fill={red} rounded />
-            <Triangle fill={green} />
+            <Triangle aspect fill={green} />
         </HStack>
     </VStack>
-</Frame>
+</Box>
